688c8d68d10db5890c32b3bfba71054cfceb016b
Deliaaan <delian.ngtmr@gmail.com>
2023-03-01 13:26:53 -0600

    programa finalizado

--- day1/app.js
+++ day1/app.js
@@ -1,3 +1,20 @@
-window.addEventListener('keydown', function(e) {
-    const audio = docuement.querySelector('audio[data-key=]');
-});
+
+function removeTransition(e){
+    if(e.propertyName !== 'transform') return; //Skip if its not a tranform
+    e.target.classList.remove('playing');
+}
+
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    if (!audio) return;
+
+    key.classList.add("playing");
+    audio.currentTime = 0;
+    audio.play();
+}
+
+
+const keys = Array.from(document.querySelectorAll(".key"));
+keys.forEach(key => key.addEventListener('transitionend', removeTransition))
+window.addEventListener('keydown', playSound);
