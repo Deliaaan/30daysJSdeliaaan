0288e8f0525002d94a833d2d7d5fcb632e4b50bf
Deliaaan <delian.ngtmr@gmail.com>
2023-03-13 09:39:12 -0600

    "start de dia 7"

--- day7/app.js
+++ day7/app.js
@@ -4,17 +4,25 @@ const people = [
 	{ name: 'Kait', year: 1986},
 	{ name: 'Irv', year: 1970},
 	{ name: 'Lux', year: 2015},
-	];
+];
 
 const comments = [
 	{ text: 'Love this', id: 523423 },
 	{ text: 'Super good', id: 823423 },
-	{ text: 'You are the best', id: 2039842 },
+	{ text: 'You are the best', id: 203984 },
 	{ text: 'Ramen is my favorite food', id: 123523 },
 	{ text: 'Nice nice nice', id: 542328 },
-	];
+];
  // some and every Checks
  // Array.prototype.some() // is at least one person 19 or older?
+const isAdult = people.some(function(person) {
+	const currenYear = (new Date()).getFullYear();
+	if (currenYear - person.year >= 19) {
+		return true;
+	} 
+});
+
+
  // Array.prototype.every() // is everyone 19 or older?
 
  // Array.prototype.find()
