13cb7e897d4b30b1448cbba32c2e6cf04da71781
Deliaaan <delian.ngtmr@gmail.com>
2023-03-13 18:56:38 -0600

    "programa 7 terminado"

--- day7/app.js
+++ day7/app.js
@@ -39,6 +39,19 @@ console.log(isNine);
  // Find is like filter, but instead returns just the one you are looking for
  // find the comment with the ID of 823423
 
+const comment = comments.filter(comment => (comment.id === 823423));
+console.log(comment);
+
  // Array.prototype.findIndex()
  // Find the comment with this ID
  // delete the comment with de ID of 823423
+
+const encontrar = comments.findIndex(comment => (comment.id === 823423));
+console.log(encontrar);
+
+//comments.splice(index, 1);
+
+const newComments = [
+	...comments.slice(0, index),
+	...comments.slice(index + 1)
+]
