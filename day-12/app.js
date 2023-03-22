69c3becef4d3452a9e8ee2a257d755692eeb4ef3
Deliaaan <delian.ngtmr@gmail.com>
2023-03-22 10:02:06 -0600

    key event listener

---
+++ day-12/app.js
@@ -0,0 +1,20 @@
+
+
+// lo convertimos en array 
+const pressed = [];
+const secretCode = 'delian'; //son 6 letras
+
+window.addEventListener('keyup', (e) => {
+    console.log(e.key);
+    pressed.push(e.key);
+
+    // Para siempre tener 6 caracteres en el array
+    // y sacar el ultimo escrito
+    pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
+
+    if (pressed.join('').includes(secretCode)) {
+        console.log("Wellcum");
+    }
+    
+    console.log(pressed);
+});
